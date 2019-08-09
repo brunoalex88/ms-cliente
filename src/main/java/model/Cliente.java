8ba01df584f8440f5fe8db2aca95453cc19d78eb
Bruno Oliveira <brunoalex_88@yahoo.com.br>
2019-08-09 18:28:35 -0300

    Commit inicial do projeto

---
+++ src/main/java/model/Cliente.java
@@ -0,0 +1,18 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private Integer idade;
+
+}
