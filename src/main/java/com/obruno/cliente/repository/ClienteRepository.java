889dce769004c75cbac35c5ab916a1e47c42e266
Bruno Oliveira <brunoalex_88@yahoo.com.br>
2019-08-10 23:02:34 -0300

    Adicionado repository e docker-compose com mysql 5.7

---
+++ src/main/java/com/obruno/cliente/repository/ClienteRepository.java
@@ -0,0 +1,10 @@
+package com.obruno.cliente.repository;
+
+import com.obruno.cliente.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+}
