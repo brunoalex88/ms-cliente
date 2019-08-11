8c72cf2751f100c0053340b37a33193e6560f5ce
Bruno Oliveira <brunoalex_88@yahoo.com.br>
2019-08-11 12:13:22 -0300

    Adicionado recursos de remover, remover por id, atualizar e buscar por id

--- src/main/java/com/obruno/cliente/controller/ClienteController.java
+++ src/main/java/com/obruno/cliente/controller/ClienteController.java
@@ -3,11 +3,14 @@ package com.obruno.cliente.controller;
 import com.obruno.cliente.model.Cliente;
 import com.obruno.cliente.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.websocket.server.PathParam;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cliente")
@@ -21,9 +24,29 @@ public class ClienteController {
         return clienteRepository.findAll();
     }
 
+    @GetMapping(value = "/{id}")
+    public Optional<Cliente> listarPorId(@PathVariable Long id) {
+        return clienteRepository.findById(id);
+    }
+
     @PostMapping
     public ResponseEntity<Cliente> adicionar(@RequestBody @Valid Cliente cliente) {
-        return ResponseEntity.ok(clienteRepository.save(cliente));
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteRepository.save(cliente));
+    }
+
+    @PutMapping
+    public ResponseEntity<Cliente> alterar(@RequestBody @Valid Cliente cliente) {
+        return ResponseEntity.status(HttpStatus.OK).body(clienteRepository.save(cliente));
+    }
+
+    @DeleteMapping
+    public void remover(@RequestBody @Valid Cliente cliente) {
+        clienteRepository.delete(cliente);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void removerPorId(@PathVariable("id") Long id) {
+        clienteRepository.deleteById(id);
     }
 
 }
